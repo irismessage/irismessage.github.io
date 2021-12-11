8d96303bc449ade939a9556316356623745637c8
JMcB <joel.mcbride1@live.com>
2021-12-11 16:24:50 +0000

    Finish writing the first article about dual booting

--- assets/dualboot/convert.py
+++ assets/dualboot/convert.py
@@ -3,11 +3,11 @@ from pathlib import Path
 
 src = '.bmp'
 dest = '.png'
-pwd = Path().resolve()
+pwd = Path(__file__).resolve().parent
 bmp = pwd / 'bmp/'
 
 for path in bmp.iterdir():
     new_path = pwd.joinpath(path.name).with_suffix(dest)
-    command = f'magick convert {path} {new_path}'
+    command = f'magick convert "{path}" "{new_path}"'
     print(command)
     os.system(command)
