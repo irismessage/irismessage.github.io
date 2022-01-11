500d41730b84584a633cc8b822feef4705f2bef0
jmcb <joelsgp@protonmail.com>
2022-01-11 19:14:28 +0000

    Startup script for linux

---
+++ blog.sh
@@ -0,0 +1,7 @@
+cd "/home/joelm/src/www/joelsgp.github.io"
+subl .
+# start bundle exec jekyll serve --unpublished --drafts
+xdg-open https://joelsgp.github.io
+xdg-open http://127.0.0.1:4000/
+xdg-open https://github.com/joelsgp/joelsgp.github.io
+
