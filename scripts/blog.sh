26bd8052a99fa814bfeca4db9ce7d4f923e741be
jmcb <joelsgp@protonmail.com>
2023-03-26 21:51:58 +0100

    More scripts

--- scripts/blog.sh
+++ scripts/blog.sh
@@ -1,8 +1,8 @@
-cd "~/src/www/joelsgp.github.io"
-code .
-# bundle exec jekyll serve --unpublished --drafts
-xdg-open "https://joelsgp.github.io"
-xdg-open "http://127.0.0.1:4000/"
-xdg-open "https://github.com/joelsgp/joelsgp.github.io"
-xdg-open "https://keep.google.com/u/0/#LIST/1C0eruu-zmek14oEGg9yu2BEtIgwVRgLftWl_L04gP-SjIeI-QpT-5ORB-x4dqw"
-
+#!/bin/sh
+cd '~/src/joelsgp.github.io'
+$VISUAL .
+./serve.sh
+xdg-open 'http://localhost:4000/'
+xdg-open 'https://joelsgp.github.io'
+xdg-open 'https://github.com/joelsgp/joelsgp.github.io'
+xdg-open 'https://keep.google.com/u/0/#LIST/1C0eruu-zmek14oEGg9yu2BEtIgwVRgLftWl_L04gP-SjIeI-QpT-5ORB-x4dqw'
---
+++ scripts/install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo pacman -Syu ruby xclip base-devel
+bundle install
